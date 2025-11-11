085e0d0630b94265a3cf4465e2df2af9c62fc932
June Shim <june.shim2@gmail.com>
2025-11-11 13:40:31 +0900

    25.11.11

---
+++ 문자열뒤집기_.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string s; cin >> s;
+    reverse(s.begin(), s.end());
+    cout << s << endl;
+    return 0;
+}
