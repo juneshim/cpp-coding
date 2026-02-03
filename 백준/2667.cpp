c5fb66008a0c5aa49cd7542e11bb740452adfb6e
June Shim <june.shim2@gmail.com>
2026-02-03 20:34:07 +0900

    26.02.03

--- 백준/2667.cpp
+++ 백준/2667.cpp
@@ -1,1 +1,64 @@
-//아직..
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+vector<vector<int>> v;
+bool visited[25][25];
+
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+int dfs(int x, int y) {
+    visited[x][y] = true;
+    int cnt = 1;
+
+    for (int d = 0; d < 4; d++) {
+        int nx = x + dx[d];
+        int ny = y + dy[d];
+
+        if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+        if (!visited[nx][ny] && v[nx][ny] == 1) {
+            cnt += dfs(nx, ny);
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+    v.assign(n, vector<int>(n));
+
+    string s;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        for (int j = 0; j < n; j++) {
+            v[i][j] = s[j] - '0';
+        }
+    }
+
+    vector<int> ans;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (!visited[i][j] && v[i][j] == 1) {
+                ans.push_back(dfs(i, j));
+            }
+        }
+    }
+
+    sort(ans.begin(), ans.end());
+
+    cout << ans.size() << '\n';
+    for (int x : ans) {
+        cout << x << '\n';
+    }
+
+    return 0;
+}
